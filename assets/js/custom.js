a5cf1cdbed93af3cc708c83fae5b149f707bdb2d
Dmitry Andriyanov <dmitryan@devart.com>
2021-09-21 12:10:23 +0300

    TS-529 Added Sales popup to posts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -31,10 +31,52 @@ $(document).ready(function () {
   // Subscription popup
   $('.btn-subscribe').click(function(event) {
     event.preventDefault();
-    $('.fader').addClass('visible');
+    $('#subscription-fader').addClass('visible');
   });
 
-  $('.fader').click(function() {
-    $('.fader').removeClass('visible');
+  $('#subscription-fader').click(function() {
+    $('#subscription-fader').removeClass('visible');
+  });
+});
+
+$(window).on('load', function(){
+
+  // Sales popup
+  let salesPopupShoved = false;
+
+  $(window).scroll(function () {
+    checkArticleScroll();
+  });
+
+  function checkArticleScroll() {
+    if(!($('.sales-anchor').length)) return;
+
+    let anchorPos = $('.sales-anchor').offset().top;
+    let topPos = $(window).scrollTop();
+    let windowHeight = $(window).height() / 2;
+
+    if (topPos + windowHeight > anchorPos && !salesPopupShoved) {
+      $('#sales-fader, .sales-popup').addClass('visible');
+      salesPopupShoved = true;
+    }
+  }
+
+  $('#sales-fader, .btn-popup-close').click(function() {
+    $('#sales-fader, .sales-popup').removeClass('visible');
+  });
+
+  // Copy sales code
+  function copySalesCode() {
+    let $tmp = $("<input>");
+    $("body").append($tmp);
+    $tmp.val($('#code-field').text()).select();
+    document.execCommand("copy");
+    $tmp.remove();
+  }
+
+  $('.sales-popup .btn-copy').click(function() {
+    copySalesCode();
+    let copiedText = 'Copied: ' + $('#code-field').html();
+    $('.sales-popup .tooltip').html(copiedText);
   });
 });
